73a736f065a28dd255c9ac480eb7b446c4ad64b8
Csaba19999 <menesi777@gmail.com>
2021-04-12 02:03:47 +0200

    mozgatásraVár

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const guild = client.guilds.cache.get(331376399950544898);
 
-client.once('ready', () => {
-	console.log('Ready!');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', msg => {
+    if (msg.content === '!thomashelp') {
+        msg.channel.send(
+                'Jelenleg elérhető parancsok' + '\n' + '\n'
+                + '- !thomashelp = Segítség kérés' + '\n'
+                + '- !thomasoltás = Thomas beszól neked valamit.' + '\n'
+                + '- !hullámvasút @username = Felültetsz vele valakit a Thomasra.'+'\n'
+                + '- !tsound "effect_száma" = Lejátszasz egy hang effectet! effectek listájához írd be a "!tsoundlist"-parancsot!');
+    }
+    else if (msg.content === '!thomasoltás') {
+        msg.channel.send('Mi kéne te fasz?');
+    }
+    else if (msg.content === '!tsoundlist') {
+        msg.channel.send('1 - bruhh '+'\n'
+                            +'2 - oh no'+'\n'
+                            +'3 - no god'+'\n'
+                            +'4 - why are you runnin'+'\n'
+                            +'5 - dam boy');
+    }
+    else if (msg.content === '!tsound 1') {
+        msg.channel.send('Fejlesztés alatt');
+    }
+    else if (msg.content.includes("!hullámvasút")) {
+        const taggedUser = msg.mentions.users.first();
+        msg.channel.send(`Thomas elvitte : ${taggedUser} egy körre.`);
+        taggedUser.voice.setChannel(`${830749958856114187}`);
+    }
+    
+
 });
 
-client.on('message', message => {
-	if (message.content === '!thomashelp') {
-		message.channel.send('Jelenleg elérhető parancsok'+'\n'+'\n'
-                        +'          - !thomashelp = Segítség kérés'+'\n'
-                        +'    - !thomasoltás = Thomas beszól neked valamit.'+'\n'
-                        +'- !hullámvasút @username = Felültetsz vele valakit a vasútra.');
-	}
-        if (message.content === '!thomasoltás') {
-		message.channel.send('Mi kéne te fasz?');
-	}
-        if (message.content === '!hullámvasút') {
-		message.channel.send('Jelenleg fejlesztés alatt áll. Térj vissza később!');
-	}
-});
-
-client.login('ODMwNzUzMzY0NDY2OTkxMTI1.YHLRXA.2ccvZSQBuVvAWXx0-EWtK4AB-Xg');
+client.login('ODMwNzUzMzY0NDY2OTkxMTI1.YHLRXA.8CDV0SaSGKH-vjiLWR-mj4ThZbQ');
---
+++ src/structures/ClientApplication.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const Team = require('./Team');
+const Application = require('./interfaces/Application');
+const ApplicationFlags = require('../util/ApplicationFlags');
+
+/**
+ * Represents a Client OAuth2 Application.
+ * @extends {Application}
+ */
+class ClientApplication extends Application {
+  _patch(data) {
+    super._patch(data);
+
+    /**
+     * The flags this application has
+     * @type {ApplicationFlags}
+     */
+    this.flags = 'flags' in data ? new ApplicationFlags(data.flags) : this.flags;
+
+    /**
+     * The app's cover image
+     * @type {?string}
+     */
+    this.cover = data.cover_image ?? this.cover ?? null;
+
+    /**
+     * The app's RPC origins, if enabled
+     * @type {string[]}
+     */
+    this.rpcOrigins = data.rpc_origins ?? this.rpcOrigins ?? [];
+
+    /**
+     * If this app's bot requires a code grant when using the OAuth2 flow
+     * @type {?boolean}
+     */
+    this.botRequireCodeGrant = data.bot_require_code_grant ?? this.botRequireCodeGrant ?? null;
+
+    /**
+     * If this app's bot is public
+     * @type {?boolean}
+     */
+    this.botPublic = data.bot_public ?? this.botPublic ?? null;
+
+    /**
+     * The owner of this OAuth application
+     * @type {?User|Team}
+     */
+    this.owner = data.team
+      ? new Team(this.client, data.team)
+      : data.owner
+      ? this.client.users.add(data.owner)
+      : this.owner ?? null;
+  }
+
+  /**
+   * Whether this application is partial
+   * @type {boolean}
+   * @readonly
+   */
+  get partial() {
+    return !this.name;
+  }
+
+  /**
+   * Obtains this application from Discord.
+   * @returns {Promise<ClientApplication>}
+   */
+  async fetch() {
+    const app = await this.client.api.oauth2.applications('@me').get();
+    this._patch(app);
+    return this;
+  }
+}
+
+module.exports = ClientApplication;
